27a16426bc5c5b5235bf34f45386d9bd53d3aab1
favini <darthjee@gmail.com>
2013-07-01 19:33:53 -0300

    Add comments models

---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ActiveRecord::Base
+  attr_accessible :body, :email, :name, :post_id
+
+  belongs_to :post
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :title
 
+  has_many :comments, :dependent => :destroy
+
   validates_presence_of :title, :body
   validates_uniqueness_of :title
 end
